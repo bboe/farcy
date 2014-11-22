f98a885008e3b15b21b74f28d4b7d9ffda0147a8
Bryce Boe <bbzbryce@gmail.com>
2014-11-21 22:14:13 -0800

    Add flake8 and pep257 handlers with some handler refactoring.

--- farcy/handlers.py
+++ farcy/handlers.py
@@ -21,9 +21,45 @@ class ExtHandler(object):
 
     Subclasses must define the extension(s) that the class provides feedback
     upon via the ``EXTENSIONS`` class method.
+
+    ``BINARY`` is the name of an executable binary to look for.
+    ``BINARY_VERSION`` is version of the binary expected.
     """
 
+    BINARY = None
+    BINARY_VERSION = None
     EXTENSIONS = []
+
+    @staticmethod
+    def execute(args):
+        """Return output of argument execution ignoring status code."""
+        try:
+            return check_output(args, stderr=DEVNULL)
+        except CalledProcessError as exc:
+            return exc.output
+
+    @classmethod
+    def verify_version(cls, installed, exact=False):
+        """Raise HandlerException if the installed version does not match.
+
+        :param installed: The installed version number.
+        :param exact: Raise HandlerException when there is not an exact
+            match. Note that 0.27.0 is considered exact to 0.27.
+        """
+        if ' ' in installed:
+            logging.getLogger(__name__).debug(
+                'Version string contains space: {0}'.format(installed))
+        exp = parse_version(cls.BINARY_VERSION)
+        inp = parse_version(installed)
+        op = None
+        if exact and exp != inp:
+            op = ''
+        elif exp > inp:
+            op = '>= '
+        if op is not None:
+            raise HandlerException(
+                'Expected {0} {1}{2}, found {installed}'
+                .format(cls.BINARY, op, cls.BINARY_VERSION, installed))
 
     def __init__(self, on_demand=False):
         """A handler's constructor is called only once upon farcy start-up.
@@ -48,7 +84,19 @@ class ExtHandler(object):
 
     def assert_usable(self):
         """Raise HandlerException if the handler is not ready for use."""
-        raise HandlerException('Base class `ExtHandler` is never usable.')
+        if self.name == 'ExtHandler':
+            raise HandlerException('Base class `ExtHandler` is never usable.')
+        if not self.BINARY:
+            raise HandlerException('{0} does not have a binary specified.'
+                                   .format(self.name))
+        try:
+            version = check_output([self.BINARY, '--version'], stderr=DEVNULL)
+        except OSError as exc:
+            if exc.errno == 2:
+                raise HandlerException('{0} is not installed'
+                                       .format(self.BINARY))
+            raise  # Unexpected and unhandled exception
+        self.verify_version(self.version_callback(version))
 
     def process(self, filename):
         """Return the complete results of the handler against the file.
@@ -67,55 +115,46 @@ class ExtHandler(object):
                 return {}
         return self._process(filename)
 
+    def version_callback(self, version):
+        """Return a parsed version string for the binary version."""
+        return version.strip()
+
+
+class Flake8(ExtHandler):
+
+    """Provides feedback for python files using flake8."""
+
+    BINARY = 'flake8'
+    BINARY_VERSION = '2.2.3'
+    EXTENSIONS = ['.py']
+
+    def _process(self, filename):
+        return self.execute([self.BINARY, filename])
+
+    def version_callback(version):
+        """Remove the extra version information."""
+        return version.split(' ', 1)[0]
+
+
+class Pep257(ExtHandler):
+
+    """Provides feedback for python files using pep257."""
+
+    BINARY = 'pep257'
+    BINARY_VERSION = '0.3.2'
+    EXTENSIONS = ['.py']
+
+    def _process(self, filename):
+        return self.execute([self.BINARY, filename])
+
 
 class Rubocop(ExtHandler):
 
     """Provides feedback for ruby files using rubocop."""
 
+    BINARY = 'rubocop'
+    BINARY_VERSION = '0.27'
     EXTENSIONS = ['.rb']
 
-    def assert_usable(self):
-        """Test that rubocop is available and its version is sufficient."""
-        try:
-            version = check_output(['rubocop', '--version'], stderr=DEVNULL)
-        except OSError as exc:
-            if exc.errno == 2:
-                raise HandlerException('rubocop is not installed')
-            raise  # Unexpected and unhandled exception
-        verify_version('rubocop', '0.27', version)
-
     def _process(self, filename):
-        try:
-            output = check_output(['rubocop', '-f', 'j', filename])
-        except CalledProcessError as exc:
-            output = exc.output
-        return json.loads(output)
-
-
-def verify_version(name, expected, installed, exact=False):
-    """Raise HandlerException if the installed version does not match.
-
-    :param name: The package name (for exception purposes)
-    :param expected: The expected version number. Is a minimum if ``exact`` is
-        false.
-    :param installed: The installed version number.
-    :param exact: Raise HandlerException when there is not an exact match. Note
-        that 0.27.0 is considered exact to 0.27.
-    """
-    installed = installed.strip()
-    if ' ' in installed:
-        logging.getLogger(__name__).debug('Version string contains space: {0}'
-                                          .format(installed))
-    exp = parse_version(expected)
-    inp = parse_version(installed)
-    op = None
-    if exact and exp != inp:
-        op = ''
-    elif exp > inp:
-        op = '>= '
-
-    if op is not None:
-        raise HandlerException('Expected {package} {op}{expected}, found '
-                               '{installed}'
-                               .format(package=name, op=op, expected=expected,
-                                       installed=installed))
+        return json.loads(self.execute([self.BINARY, '-f', 'j', filename]))
