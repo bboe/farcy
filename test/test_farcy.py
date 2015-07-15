439d7bb744ccc7bf1bf7c9b7b11a1dc519c4a1f4
Bryce Boe <bbzbryce@gmail.com>
2015-07-15 14:34:35 -0700

    Add unit tests for _compute_pfile_stats.

--- test/test_farcy.py
+++ test/test_farcy.py
@@ -6,34 +6,93 @@ from farcy import Farcy, FarcyException
 from github3 import GitHub, GitHubError
 from io import IOBase
 from mock import MagicMock, patch
+import logging
 import unittest
 
+PFILE_ATTRS = ['contents', 'filename', 'patch', 'status']
 
 MockInfo = namedtuple('Info', ['decoded'])
-MockPFile = namedtuple('PFile', ['contents', 'filename'])
+MockPFile = namedtuple('PFile', PFILE_ATTRS)
 MockResponse = namedtuple('MockResponse', ['content', 'status_code'])
+
+
+def mockpfile(**kwargs):
+    for attr in PFILE_ATTRS:
+        kwargs.setdefault(attr, None)
+    return MockPFile(**kwargs)
 
 
 class FarcyTest(unittest.TestCase):
 
     @patch('farcy.Farcy.get_session')
     @patch('farcy.UpdateChecker')
-    def _farcy_instance(self, mock_get_session, mock_update_checker):
-        farcy = Farcy('dummy', 'dummy')
+    def _farcy_instance(self, mock_get_session, mock_update_checker, **kwargs):
+        farcy = Farcy('dummy', 'dummy', **kwargs)
         self.assertTrue(mock_get_session.called)
         self.assertTrue(mock_update_checker.called)
         return farcy
 
+    @patch('farcy.added_lines')
+    def test_compute_pfile_stats__added(self, mock_added_lines):
+        mock_added_lines.return_value = {13: 10, 15: 20, 18: 100}
+        stats = {'added_files': 10, 'added_lines': 10}
+        actual = self._farcy_instance()._compute_pfile_stats(
+            mockpfile(patch='', status='added'), stats)
+        self.assertTrue(mock_added_lines.called)
+        self.assertEqual(mock_added_lines.return_value, actual)
+        self.assertEqual({'added_files': 11, 'added_lines': 13}, stats)
+
+    def test_compute_pfile_stats__excluded(self):
+        stats = {'blacklisted_files': 10}
+        farcy = self._farcy_instance(exclude_paths=['tmp/*'])
+        self.assertEqual(None, farcy._compute_pfile_stats(
+            mockpfile(filename='tmp/foo'), stats))
+        self.assertEqual({'blacklisted_files': 11}, stats)
+
+    @patch('farcy.added_lines')
+    def test_compute_pfile_stats__modified(self, mock_added_lines):
+        mock_added_lines.return_value = {1: 1, 2: 2}
+        for status in ['modified', 'renamed']:
+            stats = {'modified_files': 10, 'modified_lines': 10}
+            actual = self._farcy_instance()._compute_pfile_stats(
+                mockpfile(patch='', status=status), stats)
+            self.assertTrue(mock_added_lines.called)
+            mock_added_lines.reset_mock()
+            self.assertEqual(mock_added_lines.return_value, actual)
+            self.assertEqual({'modified_files': 11, 'modified_lines': 12},
+                             stats)
+
+    def test_compute_pfile_stats__no_change(self):
+        stats = {'unchanged_files': 10}
+        self.assertEqual(None, self._farcy_instance()._compute_pfile_stats(
+            mockpfile(status='added'), stats))
+        self.assertEqual({'unchanged_files': 11}, stats)
+
+    def test_compute_pfile_stats__removed(self):
+        stats = {'deleted_files': 10}
+        farcy = self._farcy_instance(exclude_paths=['tmp/*'])
+        self.assertEqual(None, farcy._compute_pfile_stats(
+            mockpfile(filename='a/tmp/b', status='removed'), stats))
+        self.assertEqual({'deleted_files': 11}, stats)
+
+    def test_compute_pfile_stats__unexpected_status(self):
+        logger = logging.getLogger('farcy')
+        stats = {}
+        with patch.object(logger, 'critical') as mock_critical:
+            self.assertEqual(None, self._farcy_instance()._compute_pfile_stats(
+                mockpfile(patch='', status='foobar'), stats))
+            self.assertTrue(mock_critical.called)
+        self.assertEqual({}, stats)
+
     def test_get_issues__simple_module(self):
         farcy = self._farcy_instance()
-        pfile = MockPFile(contents=lambda: MockInfo(decoded=b'"""A."""\n'),
+        pfile = mockpfile(contents=lambda: MockInfo(decoded=b'"""A."""\n'),
                           filename='a.py')
         self.assertEqual({}, farcy.get_issues(pfile))
 
     def test_get_issues__no_handlers(self):
         farcy = self._farcy_instance()
-        pfile = MockPFile(contents=None, filename='')
-        self.assertEqual({}, farcy.get_issues(pfile))
+        self.assertEqual({}, farcy.get_issues(mockpfile(filename='')))
 
     @patch('farcy.open', create=True)
     @patch('github3.authorize')
