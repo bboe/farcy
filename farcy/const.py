0ec590fcf89d8de42e85407e5eb68af990bcd633
Bryce Boe <bbzbryce@gmail.com>
2015-07-15 13:18:57 -0700

    Refactor logging messages.
    * Provide aggregate statistics about files and lines, in the logs for a pull
      request rather than a log line for each file.
    * Only display individual issues when in debug mode.

--- farcy/const.py
+++ farcy/const.py
@@ -3,13 +3,16 @@ import os
 import re
 
 __version__ = '0.1'
-NUMBER_RE = re.compile('(\d+)')
+
 VERSION_STR = 'farcy v{0}'.format(__version__)
+
+CONFIG_DIR = os.path.expanduser('~/.config/farcy')
+
 MD_VERSION_STR = ('[{0}](https://github.com/appfolio/farcy)'
                   .format(VERSION_STR))
-PR_ISSUE_COMMENT_FORMAT = '_{0}_ {{0}}'.format(MD_VERSION_STR)
-COMMIT_STATUS_FORMAT = '{0} {{0}}'.format(VERSION_STR)
 
 FARCY_COMMENT_START = '_{0}_'.format(MD_VERSION_STR)
 
-CONFIG_DIR = os.path.expanduser('~/.config/farcy')
+NUMBER_RE = re.compile('(\d+)')
+
+STATUS_FORMAT = '{0} {{0}}'.format(VERSION_STR)
