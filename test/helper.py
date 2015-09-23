f51e251b9975177830c5e6a4a8dd387857f1e3e4
Bryce Boe <bbzbryce@gmail.com>
2015-09-23 10:36:46 -0700

    Correct a few pep257 issues with the Struct class in test/helper.py.

--- test/helper.py
+++ test/helper.py
@@ -2,9 +2,14 @@
 
 
 class Struct(object):
+
+    """A dynamic class with attributes based on the input dictionary."""
+
     def __init__(self, iterable=None, **attrs):
+        """Create an instance of the Struct class."""
         self.__dict__.update(attrs)
         self._iterable = iterable or []
 
     def __getitem__(self, index):
-            return self._iterable[index]
+        """Return the value of the attribute ``index``."""
+        return self._iterable[index]
